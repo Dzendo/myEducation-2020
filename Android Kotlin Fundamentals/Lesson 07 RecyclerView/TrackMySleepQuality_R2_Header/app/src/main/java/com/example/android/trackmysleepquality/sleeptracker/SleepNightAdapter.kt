0f0a4cb1f06c35ebe8f4d653e1bff11e7cf122a8
delafalina <del.afalina.@gmail.com>
2020-07-11 21:02:51 +0300

    25 Recycler R2 Ready DiffUtil и binding Click and Grid Ready 22-26 Recycler R2 Header Ready Ready All TrackMySleepQuality_R2_Header

--- Android Kotlin Fundamentals/Lesson 07 RecyclerView/TrackMySleepQuality_R2_Header/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Android Kotlin Fundamentals/Lesson 07 RecyclerView/TrackMySleepQuality_R2_Header/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -18,29 +18,82 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * Цель этого адартеоа - составить список сна ночью и адаптировать его во что-то,
  * что переработчик может использовать для отображения на экране
  */
+//23,6 Затем, чтобы выяснить, какой тип представления вернуть, добавьте проверку,
+// чтобы увидеть, какой тип элемента находится в списке:
+private val ITEM_VIEW_TYPE_HEADER = 0
+private val ITEM_VIEW_TYPE_ITEM = 1
 
-
+//class SleepNightAdapter(private val clickListener: SleepNightListener):
+//    ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
+//23,3 Обновите объявление SleepNightAdapter для поддержки любого типа держателя представления.
+//class SleepNightAdapter:
+//    ListAdapter<SleepNight, RecyclerView.ViewHolder>(SleepNightDiffCallback())
+//23,4 Теперь давайте обновимся, ListAdapterчтобы содержать список DataItem вместо списка SleepNight:
 class SleepNightAdapter(private val clickListener: SleepNightListener):
-    ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
+    ListAdapter<DataItem, RecyclerView.ViewHolder>(SleepNightDiffCallback()) {
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-    holder.bind(clickListener, getItem(position))
-}
+    private val adapterScope = CoroutineScope(Dispatchers.Default)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-                    ViewHolder.from(parent)
 
+  //  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+  //      holder.bind(clickListener, getItem(position))
+//}
+    // 23,8 В onBindViewHolder()вам нужно разворачивать , DataItem чтобы пропускать SleepNight к ViewHolder
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+      when (holder) {
+          is ViewHolder -> {
+              val nightItem = getItem(position) as DataItem.SleepNightItem
+              holder.bind(clickListener, nightItem.sleepNight)
+          }
+      }
+    }
+// Измените тип возврата onCreateViewHolder на RecyclerView.ViewHolder и обновите его,
+// чтобы он возвращал правильный ITEM_VIEW_TYPE.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        when (viewType) {
+            ITEM_VIEW_TYPE_HEADER -> TextViewHolder.from(parent)
+            ITEM_VIEW_TYPE_ITEM -> ViewHolder.from(parent)
+            else -> throw ClassCastException("Unknown viewType ${viewType}")
+        }
+
+    //23,9 Вам понадобится способ конвертировать List<SleepNight>в List<DataItem>.
+    // Вместо того, чтобы использовать submitList(), предоставленный ListAdapter,
+    // для отправки вашего списка, вы будете использовать эту функцию,
+    // чтобы добавить заголовок, а затем отправить список.
+    fun addHeaderAndSubmitList(list: List<SleepNight>?) {
+        // запустите сопрограмму в, adapterScope чтобы управлять списком
+        adapterScope.launch {
+            //если переданный список есть null, верните только заголовок,
+            // в противном случае присоедините заголовок к заголовку списка,
+            // а затем отправьте список.
+            val items = when (list) {
+                null -> listOf(DataItem.Header)
+                else -> listOf(DataItem.Header) + list.map { DataItem.SleepNightItem(it) }
+            }
+            // Затем переключитесь в Dispatchers.Main контекст, чтобы отправить список
+            withContext(Dispatchers.Main) {
+                submitList(items)
+            }
+        }
+        // Ваш код должен собираться и запускаться, и вы не увидите никакой разницы.
+    }
 
     class ViewHolder private constructor(val binding: ListItemSleepNightBinding)
         : RecyclerView.ViewHolder(binding.root) {
@@ -58,21 +111,55 @@ class SleepNightAdapter(private val clickListener: SleepNightListener):
             }
         }
     }
+    // 23,2 Добавьте класс TextHolder внутри SleepNightAdapter класса.
+    class TextViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        companion object {
+            fun from(parent: ViewGroup): TextViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.header, parent, false)
+                return TextViewHolder(view)
+            }
+        }
+    }
+//23,6 Затем, чтобы выяснить, какой тип представления вернуть, добавьте проверку,
+// чтобы увидеть, какой тип элемента находится в списке:
+    // Затем переопределите getItemViewType и верните правильный тип представления элемента.
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is DataItem.Header -> ITEM_VIEW_TYPE_HEADER
+            is DataItem.SleepNightItem -> ITEM_VIEW_TYPE_ITEM
+        }
+    }
+
+
 }
 // Умное обновление изменившихся элементов на экране, а не всех
-class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
-    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
-        return oldItem.nightId == newItem.nightId
+//23,5 Обновите ваш DiffCallback для обработки DataItem вместо SleepNight:
+class SleepNightDiffCallback : DiffUtil.ItemCallback<DataItem>() {
+    override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+        return oldItem.id == newItem.id
     }
-    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
-       return oldItem == newItem  // проверит все поля т.к. data class SleepNight
+
+    override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+        return oldItem == newItem
     }
 }
 // Вызывается из XML при нажатии на элемент списка RecyclerView через лямбду
 class SleepNightListener(val clickListener: (sleepId: Long) -> Unit) {
     fun onClick(night: SleepNight) = clickListener(night.nightId)
 }
+// 23.1. Внизу SleepNightAdapter добавьте запечатанный класс с именем DataItem:
+sealed class DataItem {
+    data class SleepNightItem(val sleepNight: SleepNight): DataItem() {
+        override val id = sleepNight.nightId
+    }
 
+    object Header: DataItem() {
+        override val id = Long.MIN_VALUE
+    }
+
+    abstract val id: Long
+}
 /*
 sleeptracker\class SleepNightAdapter
 layout\fragment_sleep_tracker.xml
--- Android Kotlin Fundamentals/Lesson 07 RecyclerView/TrackMySleepQuality_R2_Header/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ Android Kotlin Fundamentals/Lesson 07 RecyclerView/TrackMySleepQuality_R2_Header/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -53,7 +53,8 @@ class SleepTrackerFragment : Fragment() {
         // Get a reference to the binding object and inflate the fragment views.
         // Получить ссылку на объект привязки и раздуть представления фрагментов.
         val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_sleep_tracker, container, false)
+            inflater, R.layout.fragment_sleep_tracker, container, false
+        )
 
         val application = requireNotNull(this.activity).application
 
@@ -61,7 +62,7 @@ class SleepTrackerFragment : Fragment() {
 
         val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
         val sleepTrackerViewModel =
-                ViewModelProvider(this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+            ViewModelProvider(this, viewModelFactory).get(SleepTrackerViewModel::class.java)
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
@@ -69,71 +70,93 @@ class SleepTrackerFragment : Fragment() {
         val manager = GridLayoutManager(activity, 3)
         binding.sleepList.layoutManager = manager
 
+        // 25 растяжка заголовка на три столбца:
+        //25.1 Создайте SpanSizeLookup.
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            //25,2 Используйте Alt-Enter, чтобы переопределить метод getSpanSize.
+            override fun getSpanSize(position: Int): Int =  when (position) {
+                // 25,3 Верните правильный размер диапазона для каждой позиции.
+                0 -> 3
+                else -> 1
+            }
+        }
+        // 25,4 Запустите приложение еще раз и проверьте свой заголовок из 3 столбцов!
         binding.lifecycleOwner = viewLifecycleOwner // this
 
         // Говорит можно объявить и в RecyclerView XML
         val adapter = SleepNightAdapter(SleepNightListener { nightId ->
             sleepTrackerViewModel.onSleepNightClicked(nightId)
-           // Toast.makeText(context, "${nightId}", Toast.LENGTH_LONG).show()
+            // Toast.makeText(context, "${nightId}", Toast.LENGTH_LONG).show()
         })
         binding.sleepList.adapter = adapter
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner) {
-            it?.let {  adapter.submitList(it)  }
-            // LiveData observers are sometimes passed null, so make sure you check for null.
-            // Наблюдатели живых данных иногда передаются null, поэтому убедитесь, что вы проверяете наличие null.
-        }
-
-        // Add an Observer on the state variable for showing a Snackbar message when the CLEAR button is pressed.
-        // Добавьте наблюдателя в переменную состояния для отображения сообщения закусочной при нажатии кнопки Очистить.
-        sleepTrackerViewModel.showSnackBarEvent.observe(viewLifecycleOwner) {
-            if (it == true) { // Observed state is true. Наблюдаемое состояние истинно.
-                Snackbar.make(requireActivity().findViewById(android.R.id.content),
-                        getString(R.string.cleared_message), Snackbar.LENGTH_SHORT).show()
-                // Reset state to make sure the snackbar is only shown once,
-                // even if the device has a configuration change.
-                // Сбросьте состояние, чтобы убедиться, что снэк-бар отображается только один раз,
-                // даже если устройство имеет изменение конфигурации.
-                sleepTrackerViewModel.doneShowingSnackbar()
+            it?.let {
+                //adapter.submitList(it)
+                adapter.addHeaderAndSubmitList(it)
+                // 23.10 Наконец, обновите, SleepTrackerFragment чтобы передать список DataItem вместо списка SleepNight
+                // и вызвать новый метод addHeaderAndSubmitList вместо метода submitList:
+                // LiveData observers are sometimes passed null, so make sure you check for null.
+                // Наблюдатели живых данных иногда передаются null, поэтому убедитесь, что вы проверяете наличие null.
             }
         }
 
-        // Add an Observer on the state variable for Navigating when STOP button is pressed.
-        // Добавьте наблюдателя в переменную состояния для навигации при нажатии кнопки STOP.
-        sleepTrackerViewModel.navigateToSleepQuality.observe(viewLifecycleOwner) { night ->
-            night?.let {
-                // We need to get the navController from this, because button is not ready, and it
-                // just has to be a view. For some reason, this only matters if we hit stop again
-                // after using the back button, not if we hit stop and choose a quality.
-                // Also, in the Navigation Editor, for Quality -> Tracker, check "Inclusive" for
-                // popping the stack to get the correct behavior if we press stop multiple times
-                // followed by back.
-                // Нам нужно получить навигационный контроллер от этого, потому что кнопка не готова, и она
-                // просто должен быть вид. По какой-то причине это имеет значение только в том случае, если мы снова нажмем стоп
-                // после использования кнопки назад, а не если мы нажмем стоп и выберем качество.
-                // Кроме того, в Редакторе навигации для Quality -> Tracker установите флажок "Inclusive" для
-                // выскакивание стека, чтобы получить правильное поведение, если мы нажмем стоп несколько раз
-                // за ним следует спина.
-                // Also: https://stackoverflow.com/questions/28929637/difference-and-uses-of-oncreate-oncreateview-and-onactivitycreated-in-fra
-                this.findNavController().navigate(
+            // Add an Observer on the state variable for showing a Snackbar message when the CLEAR button is pressed.
+            // Добавьте наблюдателя в переменную состояния для отображения сообщения закусочной при нажатии кнопки Очистить.
+            sleepTrackerViewModel.showSnackBarEvent.observe(viewLifecycleOwner) {
+                if (it == true) { // Observed state is true. Наблюдаемое состояние истинно.
+                    Snackbar.make(
+                        requireActivity().findViewById(android.R.id.content),
+                        getString(R.string.cleared_message), Snackbar.LENGTH_SHORT
+                    ).show()
+                    // Reset state to make sure the snackbar is only shown once,
+                    // even if the device has a configuration change.
+                    // Сбросьте состояние, чтобы убедиться, что снэк-бар отображается только один раз,
+                    // даже если устройство имеет изменение конфигурации.
+                    sleepTrackerViewModel.doneShowingSnackbar()
+                }
+            }
+
+            // Add an Observer on the state variable for Navigating when STOP button is pressed.
+            // Добавьте наблюдателя в переменную состояния для навигации при нажатии кнопки STOP.
+            sleepTrackerViewModel.navigateToSleepQuality.observe(viewLifecycleOwner) { night ->
+                night?.let {
+                    // We need to get the navController from this, because button is not ready, and it
+                    // just has to be a view. For some reason, this only matters if we hit stop again
+                    // after using the back button, not if we hit stop and choose a quality.
+                    // Also, in the Navigation Editor, for Quality -> Tracker, check "Inclusive" for
+                    // popping the stack to get the correct behavior if we press stop multiple times
+                    // followed by back.
+                    // Нам нужно получить навигационный контроллер от этого, потому что кнопка не готова, и она
+                    // просто должен быть вид. По какой-то причине это имеет значение только в том случае, если мы снова нажмем стоп
+                    // после использования кнопки назад, а не если мы нажмем стоп и выберем качество.
+                    // Кроме того, в Редакторе навигации для Quality -> Tracker установите флажок "Inclusive" для
+                    // выскакивание стека, чтобы получить правильное поведение, если мы нажмем стоп несколько раз
+                    // за ним следует спина.
+                    // Also: https://stackoverflow.com/questions/28929637/difference-and-uses-of-oncreate-oncreateview-and-onactivitycreated-in-fra
+                    this.findNavController().navigate(
                         SleepTrackerFragmentDirections
-                                .actionSleepTrackerFragmentToSleepQualityFragment(night.nightId))
-                // Reset state to make sure we only navigate once, even if the device
-                // has a configuration change.
-                // Сбросить состояние, чтобы убедиться, что мы перемещаемся только один раз, даже если устройство
-                // имеет изменение конфигурации.
-                sleepTrackerViewModel.doneNavigating()
+                            .actionSleepTrackerFragmentToSleepQualityFragment(night.nightId)
+                    )
+                    // Reset state to make sure we only navigate once, even if the device
+                    // has a configuration change.
+                    // Сбросить состояние, чтобы убедиться, что мы перемещаемся только один раз, даже если устройство
+                    // имеет изменение конфигурации.
+                    sleepTrackerViewModel.doneNavigating()
+                }
             }
-        }
-        sleepTrackerViewModel.navigateToSleepDataQuality.observe(viewLifecycleOwner) {night ->
-            night?.let {
-                this.findNavController().navigate(SleepTrackerFragmentDirections
-                    .actionSleepTrackerFragmentToSleepDetailFragment(night))
-                sleepTrackerViewModel.onSleepDataQualityNavigated()
+            sleepTrackerViewModel.navigateToSleepDataQuality.observe(viewLifecycleOwner) { night ->
+                night?.let {
+                    this.findNavController().navigate(
+                        SleepTrackerFragmentDirections
+                            .actionSleepTrackerFragmentToSleepDetailFragment(night)
+                    )
+                    sleepTrackerViewModel.onSleepDataQualityNavigated()
+                }
             }
+            return binding.root
         }
-        return binding.root
-    }
+
 /*
     Советы по отладке
 Если ваше приложение компилируется, но не работает, вот несколько вещей, которые нужно проверить:
